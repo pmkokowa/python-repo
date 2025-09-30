5ee27411953b4496a825fc2b82276251e5e51283
PMKOKOWA <pmkokowa@gmail.com>
2025-09-30 14:55:09 +0100

    new updates

--- excercises.py
+++ excercises.py
@@ -24,36 +24,53 @@
 #     user_input = input("Do you want to roll dice? (yes/no): ").lower().strip()
 #     if user_input == "yes":
 #         dice1 = random.randint(1, 6)
-#         dice2 = random.randint(1, 6)
-#         print(f"({dice1} and {dice2})")
-#     elif user_input == "no":
-#         print("Gerrout")
-#         break
-#     else:
-#         print("Invalide choice!!")
+# #         dice2 = random.randint(1, 6)
+# #         print(f"({dice1} and {dice2})")
+# #     elif user_input == "no":
+# #         print("Gerrout")
+# #         break
+# #     else:
+# #         print("Invalide choice!!")
 
 
-# -------------- REVERSE A STRING ---------------------
-# user_input = input("input anything please:  ")
-# reversed_input = user_input[::-1]
-# print("reversed input is:", reversed_input)
+# # -------------- REVERSE A STRING ---------------------
+# # user_input = input("input anything please:  ")
+# # reversed_input = user_input[::-1]
+# # print("reversed input is:", reversed_input)
 
-# ------------- REVERSE A GROUP OF WORDS ------------------
+# # ------------- REVERSE A GROUP OF WORDS ------------------
 
-statement = "Lunch break will soon be over"
-words = statement.split()
-reversed_words = words[::-1]
-reversed_statement = " ".join(reversed_words)
-print(reversed_statement)
-
-
-# ------------- REVERSE A GROUP OF WORDS ------------------ 2
-
-def reverse_words():
-    sentence = input("Enter a group of words: ")
-    words = sentence.split()
-    reversed_words = ' '.join(reversed(words))
-    print("Reversed words:", reversed_words)
+# import qrcode
+# statement = "Lunch break will soon be over"
+# words = statement.split()
+# reversed_words = words[::-1]
+# reversed_statement = " ".join(reversed_words)
+# print(reversed_statement)
 
 
-reverse_words()
+# # ------------- REVERSE A GROUP OF WORDS ------------------ 2
+
+# def reverse_words():
+#     sentence = input("Enter a group of words: ")
+#     words = sentence.split()
+#     reversed_words = ' '.join(reversed(words))
+#     print("Reversed words:", reversed_words)
+
+
+# reverse_words()
+
+
+# ---- Write a a python program that takes a URL and generate
+# a QR Code for the given URL and save QR Code in a file ----
+
+
+import qrcode
+
+data = input("Enter your URL: ")
+filename = input("Enter your prefered filename: ")
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+image = qr.make_image(fill_color="black", back_color="white")
+
+image.save(filename)
+print(f"QR Code saved as {filename}")
